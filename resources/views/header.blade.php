67f1a178038b131bbf98b9c32cf02fc4dc8f892a
XavyxS <francisco.sandoval23b@udgvirtual.udg.mx>
2024-11-28 22:35:32 -0600

    img_max_size_2

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -3,17 +3,18 @@
     <ul>
       <div>
         <li class="menu-item hidden logo-js">
-          <p><strong>JM</strong><span class="software">SOFTWARE</span></p>
+          <p><strong>JM</strong><span class="software">SOFTWARE </span><span class="version">ver1.2.2</span>
+          </p>
         </li>
       </div>
       <div class="opciones-menu">
         @if(session('name'))
-          <li class="menu-item hidden"><a href="#">Usuario: <strong>{{ session('name') }}</strong></a></li>
-          <li class="menu-item hidden"><a href="/logout">Cerrar Sesión</a></li>
+        <li class="menu-item hidden"><a href="#">Usuario: <strong>{{ session('name') }}</strong></a></li>
+        <li class="menu-item hidden"><a href="/logout">Cerrar Sesión</a></li>
         @else
-          <li class="menu-item hidden"><a href="/">Home</a></li>
-          <li class="menu-item hidden"><a href="/loginForm">Inicio de Sesión</a></li>
-          <li class="menu-item hidden"><a href="/registroForm">Registro</a></li>
+        <li class="menu-item hidden"><a href="/">Home</a></li>
+        <li class="menu-item hidden"><a href="/loginForm">Inicio de Sesión</a></li>
+        <li class="menu-item hidden"><a href="/registroForm">Registro</a></li>
         @endif
       </div>
     </ul>
